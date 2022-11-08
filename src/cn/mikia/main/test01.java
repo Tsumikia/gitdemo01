a62bc1e2311a86dd24bb1ac45b2694d510351c80
mikia <1518139129@qq.com>
2022-11-08 15:00:38 +0800

    first commit

--- src/cn/mikia/main/test01.java
+++ src/cn/mikia/main/test01.java
@@ -3,5 +3,6 @@ package src.cn.mikia.main;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
+        System.out.println("push test!");
     }
 }
