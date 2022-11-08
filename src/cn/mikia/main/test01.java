a2c8377accc2d77854c28595d7ca9363d6745a3e
mikia <1518139129@qq.com>
2022-11-08 15:04:14 +0800

    test first

--- src/cn/mikia/main/test01.java
+++ src/cn/mikia/main/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
         System.out.println("push test!");
+        System.out.println("push test!");
     }
 }
