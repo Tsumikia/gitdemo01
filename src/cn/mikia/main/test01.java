e3beef9b0fa70e191ea148ad3ba917c0b6562a0d
Tsumikia <95362199+Tsumikia@users.noreply.github.com>
2022-11-08 14:59:59 +0800

    Update test01.java
    modify from github

--- src/cn/mikia/main/test01.java
+++ src/cn/mikia/main/test01.java
@@ -2,6 +2,6 @@ package src.cn.mikia.main;
 
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello IDEA!");
     }
 }
