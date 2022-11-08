b15c5ae1bf5112d2fd4d7709490f09991cf29e20
Tsumikia <95362199+Tsumikia@users.noreply.github.com>
2022-11-08 15:09:45 +0800

    Update test01.java
    pull test second

--- src/cn/mikia/main/test01.java
+++ src/cn/mikia/main/test01.java
@@ -8,5 +8,6 @@ public class test01 {
         System.out.println("push test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("pull test second!");
     }
 }
